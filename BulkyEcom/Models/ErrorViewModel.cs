54429a8eb298f55fc99c276b59fb27bda7a9f6f3
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-19 06:32:40 +0400

    Add project files.

---
+++ BulkyEcom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyEcom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
